3f75f17b1d9b6f8a09ebbaacbcf3407781752063
minJonRing <1014080407tqr@gmail.com>
2024-08-23 15:28:29 +0800

    添加用户审核按钮

--- src/modules/user/views/list.vue
+++ src/modules/user/views/list.vue
@@ -2,7 +2,22 @@
   <div class="box">
     <ListProT ref="list" :list="list" :mixinConfig="mixinConfig" :searchParam="searchParam" :tableParam="tableParam"
       :noBtns="noBtns" :otherBtns="otherBtns" :selection="selection" @getData="getData" :jump="false"
-      @getJump="getJump" />
+      @getJump="getJump">
+      <template slot="tableBtns" slot-scope="scope">
+        <el-popconfirm class="ml10" v-if="returnBtn('verify', scope)" title="确定选择？" placement="top"
+          @confirm="handleAuto(scope.row)">
+          <el-button type="text" slot="reference" style="color: #67c23a">
+            审核
+          </el-button>
+        </el-popconfirm>
+        <el-popconfirm class="ml10" v-if="returnBtn('admin', scope)" title="确定选择？" placement="top"
+          @confirm="handleAuto(scope.row)">
+          <el-button type="text" slot="reference">
+            {{ scope.data.row.isAdmin !== 2 ? "设为管理员" : "取消管理员" }}
+          </el-button>
+        </el-popconfirm>
+      </template>
+    </ListProT>
     <!--  -->
     <el-dialog title="用户信息" :visible.sync="show" width="600px">
       <div style="padding:12px 0">
@@ -10,7 +25,7 @@
           <el-descriptions-item label="昵称">{{ userForm.nickName }}</el-descriptions-item>
           <el-descriptions-item label="账号">{{ userForm.username }}</el-descriptions-item>
           <el-descriptions-item label="邮箱">{{ userForm.email }}</el-descriptions-item>
-          <!-- <el-descriptions-item label="手机号码">18100000000</el-descriptions-item> -->
+          <el-descriptions-item label="手机号码">{{ userForm.phone }}</el-descriptions-item>
           <!--  -->
           <!-- <el-descriptions-item label="当前积分">122</el-descriptions-item>
           <el-descriptions-item label="用户等级">7</el-descriptions-item>
@@ -55,7 +70,7 @@ export default {
     };
   },
   computed: {
-    ...mapGetters([]),
+    ...mapGetters(['userInfo']),
   },
   created() {
     this.init();
@@ -94,12 +109,25 @@ export default {
           width: '100px'
         },
         {
-          label: "创建时间", formatter: (row) => {
-            return row.createTime.split('T')[0]
-          },
+          label: "创建时间",
+          // formatter: (row) => {
+          //   return row.createTime.split('T')[0]
+          // },
           width: "160px"
         },
       ];
+    },
+    returnBtn(type, scope) {
+      const { data: { row } } = scope
+      if (type === 'verify') {
+        const { isAdmin } = this.userInfo
+        return [1, 2].includes(isAdmin) && !row.isAuto
+      }
+      if (type === 'admin') {
+        const { isAdmin } = this.userInfo
+        return [1].includes(isAdmin) && row.isAdmin !== 1
+      }
+      return false
     },
     getData(data) {
       this.$emit("getData", data);
@@ -109,6 +137,9 @@ export default {
         this.userForm = data
         this.show = true
       }
+    },
+    handleAuto() {
+
     }
   },
 };
